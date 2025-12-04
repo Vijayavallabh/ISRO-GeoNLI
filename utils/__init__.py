b39087bb608864ff9ca58568f395838569d1a550
shivpatil1901 <ed21b059@smail.iitm.ac.in>
2025-12-04 14:18:04 +0000

    fixed the utils init file

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,14 +3,13 @@ Utility functions for geometric calculations and visualization.
 """
 from utils.geo_calc import GeoCalculator
 from utils.visualization import annotate_image_with_boxes
-# from utils.satellite_vqa_tools import comparison_tool, distance_tool, calculator_tool
 from utils.satellite_vqa_tools import select_object_by_rank, calculate_distance_by_indices, calculator_tool
 
 
 __all__ = [
     "GeoCalculator",
     "annotate_image_with_boxes",
-    # "comparison_tool",
-    "distance_tool",
     "calculator_tool"
+    "select_object_by_rank"
+    "calculate_distance_by_indices"
 ]
