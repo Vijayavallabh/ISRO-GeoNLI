58eb8ba67596df2f6c1efc4474e7ec2edb3aac04
shivpatil1901 <ed21b059@smail.iitm.ac.in>
2025-12-04 22:43:48 +0000

    more cleaning

--- .ipynb_checkpoints/pipeline-checkpoint.py
+++
@@ -1,7 +0,0 @@
-"""
-Compatibility shim: expose `RSPipeline` under the `pipeline` module
-so scripts that import `pipeline.RSPipeline` continue to work.
-"""
-from rs_pipeline import RSPipeline
-
-__all__ = ["RSPipeline"]
